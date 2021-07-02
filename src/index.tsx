da93e3000e842a4d8bee5c2daa909736a8546d1c
jameswilddev <jameswilddev@outlook.com>
2021-07-02 19:37:05 +0100

    Add missing service worker registration.

--- src/index.tsx
+++ src/index.tsx
@@ -5,4 +5,8 @@ import * as React from "react";
 
 import { ExampleComponent } from "./example-component";
 
+if (`serviceWorker` in navigator) {
+  navigator.serviceWorker.register(`service-worker.js`);
+}
+
 reactDom.render(<ExampleComponent />, document.getElementById(`root`));
